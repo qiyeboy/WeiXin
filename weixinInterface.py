e80b2078b304c062d35d3f722e5e288a6ef8b16d
waqxr <594675898@qq.com>
2016-03-05 23:27:15 +0800

    Update weixinInterface.py

--- weixinInterface.py
+++ weixinInterface.py
@@ -37,9 +37,9 @@ class WeixinInterface:
             
     def POST(self):        
         str_xml = web.data() #获得post来的数据
-        xml = etree.fromstring(str_xml)#进行XML解析
-        content=xml.find("Content").text#获得用户所输入的内容
-        msgType=xml.find("MsgType").text
-        fromUser=xml.find("FromUserName").text
-        toUser=xml.find("ToUserName").text
-        return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的是："+content)
+#        xml = etree.fromstring(str_xml)#进行XML解析
+#        content=xml.find("Content").text#获得用户所输入的内容
+#        msgType=xml.find("MsgType").text
+#        fromUser=xml.find("FromUserName").text
+#        toUser=xml.find("ToUserName").text
+#        return self.render.reply_text(fromUser,toUser,int(time.time()),u"我现在还在开发中，还没有什么功能，您刚才说的是："+content)
